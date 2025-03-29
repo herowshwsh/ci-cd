da54a8be437e8c4af6647ff7ba7adcfbdafc3379
溺水三千 <9673792+wsh001@user.noreply.gitee.com>
2025-03-29 20:57:56 +0800

    feat: 修改说明

---
+++ src/pages/home/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <div>这是关于CI/CD关于在github上的部署模版</div>
+
+    <div>具体可以参考.github的文件下的配置</div>
+    <div>
+      <a
+        href="https://cloud.tencent.com/developer/article/2439182?policyId=1003"
+        >相关解决配置文档</a
+      >
+    </div>
+  </div>
+</template>
+<style lang="less" scoped>
+.home {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+   
+}
+</style>
